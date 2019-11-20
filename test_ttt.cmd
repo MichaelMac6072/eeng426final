972b87b83bf7670d0ea525d06da6e3d8cd1217ae
MichaelMac6072 <michael.mcnamara@yale.edu>
2019-11-20 15:30:44 -0500

    Everything should be here

---
+++ test_ttt.cmd
@@ -0,0 +1,22 @@
+vector A ttt/chan_A/t ttt/chan_A/f
+vector A_ack ttt/chan_A/a
+vector B ttt/chan_B/t ttt/chan_B/f
+vector B_ack ttt/chan_B/a
+vector C ttt/chan_C/t ttt/chan_C/f
+vector C_ack ttt/chan_C/a
+vector D ttt/chan_D/t ttt/chan_D/f
+vector D_ack ttt/chan_D/a
+vector E ttt/chan_E/t ttt/chan_E/f
+vector E_ack ttt/chan_E/a
+vector F ttt/chan_F/t ttt/chan_F/f
+vector F_ack ttt/chan_F/a
+vector G ttt/chan_G/t ttt/chan_G/f
+vector G_ack ttt/chan_G/a
+vector H ttt/chan_H/t ttt/chan_H/f
+vector H_ack ttt/chan_H/a
+vector I ttt/chan_I/t ttt/chan_I/f
+vector I_ack ttt/chan_I/a
+vector
+
+h Vdd
+l GND
